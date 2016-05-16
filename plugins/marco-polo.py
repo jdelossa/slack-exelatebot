dc5740eeb703e542319e138c3cf5fba611acc04e
Joannadelossantos@outlook.com <joannadelossantos@Joannas-MacBook-Pro.local>
2016-05-15 21:17:53 -0400

    added greetings, marco, polo

--- plugins/marco-polo.py
+++ plugins/marco-polo.py
@@ -2,22 +2,37 @@ import time
 import requests
 import shutil
 import re
-#import catch
 crontable = []
 outputs = []
 from slackclient import SlackClient
 
 def process_message(data):
-        token = "xoxb-43078106134-1CvJViGymfBRYF0r5q1I5GYc"    # My Slack Token
+        token = "xoxo"    # Your Slack Token
         sc = SlackClient(token)
+        print sc.api_call("users.info",user=data['user'])
+        print sc.api_call("channels.info",channel=data['channel'])
 
-        Marco = ['marco', 'Marco']
+        Greetings = ['Hi', 'Hello', 'hi', 'hello', 'HI', 'HELLO']
+        marco = ['Marco']
+        polo = ['Polo']
 
-        for i in Marco:
+        # Say Hello
+        for greeting in Greetings:
 
-                if i in data['text']:
-                        sendstring = "polo"
+                if greeting in data['text']:
+                        sendstring = "Say Marco"
+                        outputs.append([data['channel'], sendstring])
+        # Say marco
+        for m in marco:
+
+                if m in data['text'] or m.lower() in data['text'] or m.upper() in data['text']:
+                        sendstring = "Polo!\nSay it again"
+                        outputs.append([data['channel'], sendstring])
+
+        # Say polo
+        for p in polo:
+
+                if p in data['text'] or p.lower() in data['text'] or p.upper() in data['text']:
+                        sendstring = "Marco!\nBut next time say Marco"
                         outputs.append([data['channel'], sendstring])
                 break
-
-        #catch_all(data)
